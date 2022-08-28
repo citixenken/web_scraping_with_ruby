bbf96cec93b45087fab98cce43436114909713fe
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-28 18:00:06 +0300

    Parsing HTML with Nokogiri

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,8 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 require 'HTTParty'
-# require 'awesome_print'
+require 'nokogiri'
+require 'awesome_print'
 
 url = 'https://en.wikipedia.org/wiki/Grigori_Perelman'
 # url = 'https://data.cityofnewyork.us/resource/uvks-tn5n.json'
@@ -30,4 +31,15 @@ url = 'https://en.wikipedia.org/wiki/Grigori_Perelman'
 # using open-uri
 # ==============
 html = URI.open(url) #File descriptor => allows us to read from URL as if it were a file, line by line
-puts html 
+# puts html 
+
+#parsing HTML with Nokogiri
+#==========================
+doc = Nokogiri::HTML(html)
+# puts doc
+# description = doc.css("p").text.split("\n").find { |s| s.length > 0 } #first non-empty paragraph
+# description = doc.css("p")[1].children[1]
+# description = doc.css('img')[0]
+description = doc.css('.infobox-image img')
+
+puts description
