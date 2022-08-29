ef975c1dd1ef4f193510c27a3a9e87043ef665d2
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-29 06:03:14 +0300

    TODO: driver_path issues

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,16 @@
+# Part I: Static Pages
+# ====================
 require 'open-uri'
 require 'net/http'
 require 'json'
 require 'HTTParty'
 require 'nokogiri'
 require 'awesome_print'
+require 'csv'
 
-url = 'https://en.wikipedia.org/wiki/Grigori_Perelman'
+# url = 'https://en.wikipedia.org/wiki/Grigori_Perelman'
 # url = 'https://data.cityofnewyork.us/resource/uvks-tn5n.json'
+url = 'https://en.wikipedia.org/wiki/Douglas_Adams'
 
 # using net/http
 # ==============
@@ -33,13 +37,25 @@ url = 'https://en.wikipedia.org/wiki/Grigori_Perelman'
 html = URI.open(url) #File descriptor => allows us to read from URL as if it were a file, line by line
 # puts html 
 
-#parsing HTML with Nokogiri
-#==========================
+#parsing HTML into a DOM tree with Nokogiri
+#==========================================
 doc = Nokogiri::HTML(html)
 # puts doc
-# description = doc.css("p").text.split("\n").find { |s| s.length > 0 } #first non-empty paragraph
+description = doc.css("p").text.split("\n").find { |s| s.length > 0 } #first non-empty paragraph
 # description = doc.css("p")[1].children[1]
 # description = doc.css('img')[0]
-description = doc.css('.infobox-image img')
+# description = doc.css('.infobox-image img')
+picture = doc.css("td a img").find{|picture| picture.attributes["alt"].value.include?("Douglas adams portrait cropped.jpg")}.attributes["src"].value
 
-puts description
+# puts description
+# puts picture
+
+# Exporting scraped data to CSV
+# =============================
+
+data_arr = []
+data_arr.push(description, picture)
+
+CSV.open('data.csv', 'w') { |csv| csv << data_arr} #write data to CSV file
+
+
